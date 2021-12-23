70e346c233d94881f822e39b4049da0525ea55f4
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2021-12-23 08:49:47 -0300

    Removed Fields from UnitOptions, as Newtonsoft can already do the same thing

--- Models/Options/GraphInterfaceUnitOptions.cs
+++ Models/Options/GraphInterfaceUnitOptions.cs
@@ -1,10 +1,6 @@
-using System.Collections.Generic;
 using GraphInterface.Models.Abstract;
 
 namespace GraphInterface.Models.Options
 {
-    public class GraphInterfaceUnitOptions : GraphInterfaceRequestOptions
-    {
-        public IEnumerable<GraphInterfaceUnitField> Fields { get; set; } = null;
-    }
+    public class GraphInterfaceUnitOptions : GraphInterfaceRequestOptions {}
 }
